7b483f36d8863ab286851be837ae691f4effe436
Pierre Sinquin <159269829+Pierre-51@users.noreply.github.com>
2024-06-26 15:31:54 +0100

    :bug: fix of molstar

--- search_pdb.py
+++ search_pdb.py
@@ -24,7 +24,7 @@ def search(uniprot_id, api):
 
     except (requests.RequestException, ValueError) as e:
         if '-' in uniprot_id:
-            return search(uniprot_id[:uniprot_id.index("-")], api)
+            return search(uniprot_id[:uniprot_id.index('-')], api)
         else:
             return None
 
@@ -32,7 +32,7 @@ def download_file(url, id, location):
     try:
         response = requests.get(url)
         response.raise_for_status()
-        name = url.split("/")[-1].split("?")[0]
+        name = url.split('/')[-1].split('?')[0]
         file_path = Path(location) / f'{id}{name[-4:]}'
         with file_path.open('wb') as f:
             f.write(response.content)
@@ -42,30 +42,28 @@ def download_file(url, id, location):
 
 def extract_information(best_structure):
     chain_ids = []
-    if "entities" in best_structure:
-        for entity in best_structure["entities"]:
-            if "chain_ids" in entity:
-                chain_ids.extend(entity["chain_ids"])
+    if 'entities' in best_structure:
+        for entity in best_structure['entities']:
+            if 'chain_ids' in entity:
+                chain_ids.extend(entity['chain_ids'])
     else:
-        if "chain_id" in best_structure:
-            chain_ids.append(best_structure["chain_id"])
+        if 'chain_id' in best_structure:
+            chain_ids.append(best_structure['chain_id'])
     extracted_data = {
-        "model_identifier": best_structure.get("model_identifier"),
-        "model_category": best_structure["model_category"],
-        "model_url": best_structure.get("model_url"),
-        "pipeline_version": best_structure.get("pipeline_version"),
-        "chain": chain_ids,
-        "resolution": best_structure.get("resolution"),
-        "coverage": best_structure.get("coverage"),
-        "method": best_structure.get("experimental_method") if "experimental_method" in best_structure else 'null'
+        'model_identifier': best_structure.get('model_identifier'),
+        'model_category': best_structure['model_category'],
+        'model_url': best_structure.get('model_url'),
+        'pipeline_version': best_structure.get('pipeline_version'),
+        'chain': chain_ids,
+        'resolution': best_structure.get('resolution'),
+        'coverage': best_structure.get('coverage'),
+        'method': best_structure.get('experimental_method') if 'experimental_method' in best_structure else 'null'
     }
     return extracted_data
 
 def process_uniprot_id(uniprot_id, output_dir, version):
     global new_model_url, best_structure
-    json_file_url = f"{S3_API}{uniprot_id}.json"
-    file_path = Path(output_dir) / 'files'
-    file_path.mkdir(parents=True, exist_ok=True)
+    json_file_url = f'{S3_API}{uniprot_id}.json'
 
     try:
         response = requests.get(json_file_url)
@@ -77,13 +75,13 @@ def process_uniprot_id(uniprot_id, output_dir, version):
         saved_model_url = None
         saved_version = None
 
-    video_exists = all(requests.head(f"{S3_API}{uniprot_id}.{extension}").status_code == 200
+    video_exists = all(requests.head(f'{S3_API}{uniprot_id}.{extension}').status_code == 200
                        for extension in ['webm', 'mov'])
     process = True
     structures_alpha = search(uniprot_id, ALPHAFOLD_API)
     if structures_alpha:
         best_structure = structures_alpha[0].get('summary')
-        new_model_url = best_structure.get("model_url")
+        new_model_url = best_structure.get('model_url')
     else:
         structures_pdb = search(uniprot_id, PDBE_API)
         if structures_pdb:
@@ -97,15 +95,20 @@ def process_uniprot_id(uniprot_id, output_dir, version):
             if structures_3d:
                 structures_3d.sort(key=lambda s: s.get('summary').get('coverage'), reverse=True)
                 best_structure = structures_3d[0].get('summary')
-                new_model_url = best_structure.get("model_url")
+                new_model_url = best_structure.get('model_url')
             else:
                 process = False
-                no_structure_path = Path(output_dir) / 'files' / f'NoStructure_{uniprot_id}.txt'
-                with open(no_structure_path, 'a') as no_structure:
-                    no_structure.write(f"{uniprot_id}\n")
+                if requests.head(f'{S3_API}NoStructure_{uniprot_id}.txt').status_code != 200:
+                    file_path = Path(output_dir) / 'files'
+                    file_path.mkdir(parents=True, exist_ok=True)
+                    no_structure_path = Path(output_dir) / 'files' / f'NoStructure_{uniprot_id}.txt'
+                    with open(no_structure_path, 'a') as no_structure:
+                        no_structure.write(f'{uniprot_id}\n')
 
     if process:
         if new_model_url != saved_model_url or version != saved_version or not video_exists:
+            file_path = Path(output_dir) / 'files'
+            file_path.mkdir(parents=True, exist_ok=True)
             json_file_path = Path(output_dir) / 'files' / f'{uniprot_id}.json'
             download_file(new_model_url, uniprot_id, output_dir)
             best_structure['pipeline_version'] = version
